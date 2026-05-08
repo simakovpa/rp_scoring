34c909ea4e346171d37d4b348f6b7a434180caaf
simakovpa <toli4@outlook.com>
2026-05-08 13:12:54 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/scoring-prototype/',
+  base: '/rp_scoring/',
   build: {
     rollupOptions: {
       output: {
